4f8cabe8f2f4958b57e7bcacaa6f80d8adf9c345
Manuel <manusieirof@gmail.com>
2026-05-14 13:34:42 +0200

    fix supabase singleton

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,13 @@
 import { createClient as createSupabaseClient } from '@supabase/supabase-js'
 
+let client: any = null
+
 export function createClient() {
-  return createSupabaseClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+  if (!client) {
+    client = createSupabaseClient(
+      process.env.NEXT_PUBLIC_SUPABASE_URL!,
+      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+    )
+  }
+  return client
 }
