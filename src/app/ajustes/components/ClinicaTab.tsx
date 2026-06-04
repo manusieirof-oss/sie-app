a93020eea70a199fa727681172c8b68b8e30e86a
Manuel <manusieirof@gmail.com>
2026-06-04 20:37:53 +0200

    fix claves agenda_inicio agenda_fin en ClinicaTab

--- src/app/ajustes/components/ClinicaTab.tsx
+++ src/app/ajustes/components/ClinicaTab.tsx
@@ -9,8 +9,8 @@ export default function ClinicaTab({ ajustes, set }: any) {
           <label>Nombre de la clínica</label>
           <input className="input" value={ajustes.clinica_nombre||''} onChange={e=>set('clinica_nombre',e.target.value)} placeholder="SIE Clínica"/>
         </div>
-        <div className="field"><label>Hora de apertura</label><input className="input" type="time" value={ajustes.clinica_horario_inicio||'08:30'} onChange={e=>set('clinica_horario_inicio',e.target.value)}/></div>
-        <div className="field"><label>Hora de cierre</label><input className="input" type="time" value={ajustes.clinica_horario_fin||'21:30'} onChange={e=>set('clinica_horario_fin',e.target.value)}/></div>
+        <div className="field"><label>Hora de apertura</label><input className="input" type="time" value={ajustes.agenda_inicio||'08:00'} onChange={e=>set('agenda_inicio',e.target.value)}/></div>
+        <div className="field"><label>Hora de cierre</label><input className="input" type="time" value={ajustes.agenda_fin||'21:30'} onChange={e=>set('agenda_fin',e.target.value)}/></div>
         <div className="field"><label>Duración de la clase (minutos)</label><input className="input" type="number" value={ajustes.clinica_duracion_clase||'50'} onChange={e=>set('clinica_duracion_clase',e.target.value)}/></div>
         <div className="field"><label>Tiempo de cambio entre grupos (minutos)</label><input className="input" type="number" value={ajustes.clinica_tiempo_cambio||'10'} onChange={e=>set('clinica_tiempo_cambio',e.target.value)}/></div>
         <div className="field"><label>Máximo personas por sala</label><input className="input" type="number" value={ajustes.clinica_max_personas_sala||'6'} onChange={e=>set('clinica_max_personas_sala',e.target.value)}/></div>
